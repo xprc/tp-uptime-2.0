9ec71f65e7dedd0df13ee43cccd157c4215966d5
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-08-12 22:03:32 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,36 +1,45 @@
 window.Config = {
-
-  // 显示标题
-  SiteName: 'Public Status',
-
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only
+  SiteName: '站点实时状态 | Toms Project',
+  SiteUrl: '/',
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm785645577-71377351abf9c8f4b0ccaa7d',  // www.projectoms.com
+    'm785645636-b785f7f1f58f6fbc6d4ddcb7',  // blog.projectoms.com
+    'm785645570-f33846fc8e8e539fb5c70a6f',  // tp.je
+    'm785645572-5f0d23df38d30fba38f5d190',  // vtp.xyz
+    'm787267001-39390c9993ee33d72ccf44fd',  // API 1
+    'm787267004-bc7a2e5c107bb8177d401436',  // API 2
+    'm786806247-6ed2645a58e211b9603d5b8d',  // assets.projectoms.com
+    'm786806246-b4efa7e00cfd71ac8e655147',  // avatar.projectoms.com
+    'm788295568-76778a8263e9c19a14b4588d',  // bucket.projectoms.com
+    'm788295575-83f4ce0c67de8beb34f97a35',  // dl.projectoms.com
+    'm786806244-62a06340b5018360d5087335',  // s.tp.je
+    'm791215570-9aca9f7901f8fd11fd6a0be3',  // ns1.vtp.xyz
+    'm787703405-55e2fc68a732f011ffc471f8',  // dns1.vtp.xyz
+    'm787703473-a9fa1929268b3aa17a299511',  // dns3.vtp.xyz
+    'm786806242-378557f05cb42b9835625c0c',  // www.ckbctc.com
+    'm785645634-ebdd2fac2256264ba66b52f0',  // test.projectoms.com
+    'm786806243-daeebb121485cc8c30336092',  // cloud.tp.je
+    'm786806245-6631c88a3258952f1e66119e',  // pan.projectoms.com
+    'm785645618-c1cf8231edd7ed4957815b6b',  // github.com
+    'm785645615-35ba16df16dc1705e345bfcf',  // www.google.com
+    'm785645622-09fc5a02e4399c639360bb52',  // facebook.com
+    'm785645614-4517c604ad5fc58ca79094e7',  // www.baidu.com
+    'm785645631-8c0b32c0dc50ab657636d002'   // qq.com
   ],
-
-  // 日志天数
-  CountDays: 90,
-
-  // 是否显示检测站点的链接
   ShowLink: true,
-
-  // 导航栏菜单
+  CountDays: 60,
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '首页',
+      url: 'https://www.projectoms.com/'
+    },
+    {
+      text: '导航页',
+      url: 'https://www.tp.je/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
-  ],
+      url: 'https://github.com/ytmo'
+    }
+  ]
 };
