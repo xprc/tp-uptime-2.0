583321eeaea71365d8e28134ecbc98ef7545b8b2
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-08-12 22:16:29 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -24,7 +24,7 @@ function App() {
         </div>
         <div id='footer'>
           <p>基于 <Link to='https://uptimerobot.com/' text='UptimeRobot' /> 接口制作，检测频率 5 分钟</p>
-          <p>&copy; 2020 <Link to='https://status.org.cn/' text='STATUS.ORG.CN' />, Version {Package.version}</p>
+          <p>&copy; 2022 <Link to='https://www.projectoms.com/' text='TomsProject' /> 版权所有 v{Package.version}</p>
         </div>
       </div>
     </>
