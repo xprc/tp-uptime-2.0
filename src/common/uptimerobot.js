bd82dcf7378fa0487ca9d099751350c86bdf4c49
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-08-18 21:37:53 +0800

    Update uptimerobot.js

--- src/common/uptimerobot.js
+++ src/common/uptimerobot.js
@@ -28,9 +28,9 @@ export async function GetMonitors(apikey, days) {
   const response = await axios.post('https://status.projectoms.com/v2/getMonitors', postdata, { timeout: 10000 });
   if (response.data.stat !== 'ok')
   {
-    if (response.status == 429)
+    if (response.status === 429)
     {
-      console.log("Too Many requst!");
+      throw "Too Many requst!;
     }
     else
     {
