5b64194b35ebb68ec1903b1f867828aaea99d609
TomsProject <44722363+TomsProject@users.noreply.github.com>
2023-02-02 15:14:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app';
-import 'react-tooltip/dist/react-tooltip.css';
 import './app.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
