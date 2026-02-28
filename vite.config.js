13c30ac135fa15719b8beafdb852421a6318b688
TomsProject <44722363+TomsProject@users.noreply.github.com>
2026-02-28 21:06:23 +0800

    add support for old browser

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import legacy from "@vitejs/plugin-legacy";
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    legacy({
+      targets: [
+        'cover 99% in CN',
+        'not dead',
+        'not op_mini all',
+      ],
+    }),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
