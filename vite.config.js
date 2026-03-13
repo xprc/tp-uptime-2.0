937231756b7c12652476dda16cbd792c11e06f2f
TomsProject <44722363+TomsProject@users.noreply.github.com>
2026-03-13 21:23:07 +0800

    Add babel plugin and reactCompilerPreset to config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import legacy from "@vitejs/plugin-legacy";
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
